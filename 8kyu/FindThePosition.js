4141c757fc20212572b87a2c28123e8b0eca5155
Lix Wxit <zinebbendjafer9@gmail.com>
2022-07-15 18:13:33 +0100

    solved

---
+++ 8kyu/FindThePosition.js
@@ -0,0 +1,6 @@
+//When provided with a letter, return its position in the alphabet.
+
+function position(letter) {
+    let output = letter.charCodeAt(letter) - 96;
+           return "Position of alphabet:" + " " + output;
+  }
