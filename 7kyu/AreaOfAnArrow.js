63cd0e1547a5e5bafab51eca2d3758cda700fef3
Lix Wxit <zinebbendjafer9@gmail.com>
2022-07-26 17:47:21 +0100

    solved

---
+++ 7kyu/AreaOfAnArrow.js
@@ -0,0 +1,9 @@
+/* 
+An arrow is formed in a rectangle with sides a and b by joining the bottom corners 
+to the midpoint of the top edge and the centre of the rectangle.
+Write a function which returns the area of the arrow.
+*/
+
+function arrowArea(a, b) {
+    return (a * b) / 2 / 2;
+  }
